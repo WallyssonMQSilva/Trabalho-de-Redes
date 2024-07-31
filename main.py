82849be6019ed8e92a9f0431860e4a207cddec46
luizfd <luizfelipeducat@gmail.com>
2024-07-30 21:59:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from subprocess import Popen
+import shutil
+
+
+Popen('python node.py')
+
+print("teste1")
+
+input()
+
+Popen('python node2.py')
+
+print("teste2")
+
+input()
---
+++ node.py
@@ -0,0 +1,6 @@
+from Network.NetworkNode import NetworkNode
+
+folder = input("insira o endereço pasta a ser sincronizada (node 1): ")
+
+n = NetworkNode(folder, 1)
+
